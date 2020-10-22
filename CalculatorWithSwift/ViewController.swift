dc61c73c50ae2dee66b4a81921ee8bafc1a202e6
Peter Stoenchev <peter.stoenchev@abv.bg>
2020-10-22 23:04:34 +0300

    I'm implement new function enter number, function add numbers, and clear pad.

--- CalculatorWithSwift/ViewController.swift
+++ CalculatorWithSwift/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+// MARK: - Calculator View
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    private var userInput = ""
+    var fisrt = ""
+    var second = ""
+    var function = ""
+    var result = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - enter numbers from 0 to 9 
+    @IBAction func numbers(_ sender: UIButton) {
+        userInput += String(sender.tag)
+        numberLabel.text = userInput
+    }
+    // MARK: - Clear Text Label
+    @IBAction func deleteBtn(_ sender: UIButton) {
+        userInput = ""
+        numberLabel.text = "0"
+        
+    }
+    // MARK: See result afrer operation
+    @IBAction func equalBtn(_ sender: Any) {
+        second = userInput
+        var fisrtInput = 0.0
+        var secondInput = 0.0
+        fisrtInput = Double(fisrt)!
+        secondInput = Double(second)!
+        
+        if function == "+" {
+            result = fisrtInput + secondInput
+            numberLabel.text = String(result)
+        }
+    }
+    
+    // MARK: - Add two number, example a + b = c
+    @IBAction func addBtn(_ sender: Any) {
+        fisrt = userInput
+        function = "+"
+        userInput = ""
+        
+    }
 }
 
